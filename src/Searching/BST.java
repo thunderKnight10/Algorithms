f8aaa406ba26b250a196f77ad0767e4377391dd1
Aditya10-maX <adityasur825@gamil.com>
2026-08-15 09:35:28 +0600

    adding files

---
+++ src/Searching/BST.java
@@ -0,0 +1,9 @@
+package Searching;
+import java.util.TreeSet;
+
+public class BST{
+    public static boolean search(Node root , int val){
+       
+        while(tree)
+    }
+}
---
+++ src/Searching/FirstOccurence.java
@@ -0,0 +1,54 @@
+
+package Searching;
+
+import java.util.Scanner;
+
+public class FirstOccurence{
+    public static int search(int[] arr,int num){
+
+        int left=0;
+        int right=arr.length-1;
+        int res =-1;
+
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(arr[mid]==num){
+                res=mid;
+                right=mid-1;
+            }
+
+            else if(arr[mid]>num) right=mid-1;
+
+            else left=mid+1;
+
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Fill the array with sorting  numbers:");
+        for (int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the number you want to find:");
+        int m = sc.nextInt();
+
+        int res = search(arr, m);
+
+        if (res == -1) System.out.println("No element found.");
+        else System.out.printf("The number %d is at index %d", m, res);
+
+        sc.close();
+    }
+
+}
